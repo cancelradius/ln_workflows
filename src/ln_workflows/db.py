3dfa71771ea1bbe8b702e6f526841604fd18d2ec
felix <felix.rotter@mail.utoronto.ca>
2026-08-02 17:49:53 -0400

    feat: add WorkflowResult schema for database entries
    Replace functions which build valid entries with a Pydantic schema, add basic logging fields for a workflow. Add a WorkflowResultStore class inheriting from MongoURIStore easily extensible with an update_results method specifically for results validation and dumping.

--- src/ln_workflows/db.py
+++ src/ln_workflows/db.py
@@ -1,11 +1,19 @@
-from maggma.stores import MongoStore
-from monty.json import jsanitize
+import os
 
-def add_results(results: dict, db_name: str = "ln_workflows", db_collection: str = "results") -> None:
-    db = MongoStore(
-        database=db_name,
-        collection=db_collection,
-        username=os.
-        host="localhost",
-        port=27017,
-    )
+from maggma.stores import MongoURIStore
+from pydantic import TypeAdapter
+
+from ln_workflows.schema import WorkflowResult
+
+
+class WorkflowResultStore(MongoURIStore):
+    def __init__(self, database: str = "ln_workflows", collection: str = "results"):
+        self.uri = os.environ["MONGO_URI"]
+        super().__init__(self.uri, collection_name=collection, database=database)
+
+    def update_results(self, results: list[WorkflowResult]) -> None:
+        self.connect(force_reset=False)
+        serialized: list[dict] = TypeAdapter(list[WorkflowResult]).dump_python(
+            results, mode="json"
+        )
+        self.update(serialized, key="_id")
---
+++ src/ln_workflows/schema.py
@@ -0,0 +1,20 @@
+import os
+import uuid
+from datetime import datetime, timedelta
+
+from pydantic import UUID4, BaseModel, ConfigDict, Field, computed_field
+
+USER = os.environ.get("USER") or "unknown"
+PL_DATETIME = datetime.fromordinal(1)
+
+class WorkflowResult(BaseModel):
+    model_config = ConfigDict(frozen=True)
+    _id: UUID4 = Field(default_factory=uuid.uuid4)
+    user: str = Field(default=USER)
+    started: datetime = Field(default=PL_DATETIME)
+    finished: datetime = Field(default_factory=datetime.now)
+
+    @computed_field
+    @property
+    def walltime(self) -> timedelta:
+        return self.finished - self.started
