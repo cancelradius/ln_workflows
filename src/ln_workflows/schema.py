9ecd61bf103571718eaf599231e4f1ca513eebdd
felix <felix.rotter@mail.utoronto.ca>
2026-08-02 20:59:22 -0400

    fix: remove leading underscore on WorkflowResult id field
    Remove a leading underscore on the id field in the WorkflowResult schema to prevent issues with pydantic Fields.

--- src/ln_workflows/schema.py
+++ src/ln_workflows/schema.py
@@ -8,8 +8,12 @@ USER = os.environ.get("USER") or "unknown"
 PL_DATETIME = datetime.fromordinal(1)
 
 class WorkflowResult(BaseModel):
-    model_config = ConfigDict(frozen=True)
-    _id: UUID4 = Field(default_factory=uuid.uuid4)
+    model_config = ConfigDict(
+        frozen=True,
+        populate_by_name=True,
+        serialize_by_alias=True,
+    )
+    id: UUID4 = Field(default_factory=uuid.uuid4, alias="_id")
     user: str = Field(default=USER)
     started: datetime = Field(default=PL_DATETIME)
     finished: datetime = Field(default_factory=datetime.now)
